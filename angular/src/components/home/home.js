218fe615e1cba7ff2e24482239b7a912ef6c9b54
bryce.fisher <bryce.fisher@gmail.com>
2017-02-08 10:30:16 -0700

    Added new background and delivery preference.

--- angular/src/components/home/home.js
+++
@@ -1,8 +0,0 @@
-angular
-    .module('rising')
-    .component('home', {
-        templateUrl: 'components/home/home.html',
-        controller: function () {
-            console.log('Initializing Home Controller...');
-        }
-    });
